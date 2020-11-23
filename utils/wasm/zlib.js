a224e0dd52344623e809edeb7ad636e70f1fa24d
Mathis Mensing <matmen@dreadful.tech>
2020-11-23 17:32:36 +0100

    mmm, great debugging

--- utils/wasm/zlib.js
+++ utils/wasm/zlib.js
@@ -1,7 +1,6 @@
 let wasm;
 
 {
-    console.log(import.meta);
     const module = new WebAssembly.Module(await fetch('https://github.com/matmen/ImageScript/raw/deno/utils/wasm/zlib.wasm').then(r => r.arrayBuffer()));
     const instance = new WebAssembly.Instance(module);
 
