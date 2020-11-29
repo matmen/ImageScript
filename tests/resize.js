1405e7edb23d9658038f6b7d611a1c0613c77f81
Mathis Mensing <matmen@dreadful.tech>
2020-11-29 16:01:52 +0100

    resize test

---
+++ tests/resize.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+const {Image} = require('../ImageScript');
+(async () => {
+    {
+        const binary = await fs.readFile('./tests/image.png');
+        const image = await Image.decode(binary);
+        image.resize(image.width / 4, Image.RESIZE_AUTO);
+
+        const encoded = await image.encode();
+        const target = await fs.readFile('./tests/resize.png');
+        if (!Buffer.from(target).equals(Buffer.from(encoded))) process.exit(1);
+    }
+})();
