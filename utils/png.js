b9cbecbf765f1ca8536743f1a8a81026cd7a53a9
Mathis Mensing <matmen@dreadful.tech>
2020-12-03 14:21:07 +0100

    png: IDX support
    (cherry picked from commit 9a9a8e79b4efa392661b921a559c366f5e29ffd7)

--- utils/png.js
+++ utils/png.js
@@ -73,7 +73,8 @@ export async function decode(array) {
     const width = view.getUint32(16);
     const height = view.getUint32(20);
     const bpc = array[24];
-    const channels = ({2: 3, 6: 4, 0: 1, 4: 2})[array[25]];
+    const pixel_type = array[25];
+    let channels = ({3: 1, 0: 1, 4: 2, 2: 3, 6: 4})[pixel_type];
     const bytespp = channels * bpc / 8;
 
     const row_length = width * bytespp;
@@ -85,6 +86,9 @@ export async function decode(array) {
     let c_offset = 33;
     const chunks = [];
 
+    let palette;
+    if (array[25] === 3)
+        palette = new Uint32Array(2 ** bpc);
     let type;
     while (type !== 1229278788) {
         type = view.getUint32(4 + c_offset);
@@ -92,6 +96,10 @@ export async function decode(array) {
         // IDAT
         if (type === 1229209940)
             chunks.push(array.subarray(8 + c_offset, 8 + c_offset + view.getUint32(c_offset)));
+        else if (type === 1347179589) {
+            for (let pxlOffset = 0; pxlOffset < palette.length * 8; pxlOffset += 3)
+                palette[pxlOffset / 3] = array[8 + c_offset + pxlOffset] << 24 | array[8 + c_offset + pxlOffset + 1] << 16 | array[8 + c_offset + pxlOffset + 2] << 8 | 0xff;
+        }
 
         c_offset += 4 + 4 + 4 + view.getUint32(c_offset);
     }
@@ -111,24 +119,33 @@ export async function decode(array) {
         p_offset += row_length;
     }
 
-    if (bpc !== 8) {
-            const newPixels = new Uint8Array(pixels.length / bpc * 8);
-            for (let i = 0; i < pixels.length; i += 2)
-                newPixels[i / 2] = pixels[i];
-            pixels = newPixels;
-        }
+    if (channels === 1 && palette) {
+        channels = 4;
+        const newPixels = new Uint8Array(width * height * 4);
+        const pixelView = new DataView(newPixels.buffer, newPixels.byteOffset, newPixels.byteLength);
+        for (let i = 0; i < pixels.length; i++)
+            pixelView.setUint32(i * 4, palette[pixels[i]], false);
+        pixels = newPixels;
+    }
 
-        if (channels !== 4) {
+    if (bpc !== 8) {
+        const newPixels = new Uint8Array(pixels.length / bpc * 8);
+        for (let i = 0; i < pixels.length; i += 2)
+            newPixels[i / 2] = pixels[i];
+        pixels = newPixels;
+    }
+
+    if (channels !== 4) {
         const newPixels = new Uint8Array(width * height * 4);
         const view = new DataView(newPixels.buffer);
 
-            if (channels === 1) {
-                for (let i = 0; i < width * height; i++) {
-                    const pixel = pixels[i];
-                    view.setUint32(i * 4, pixel << 24 | pixel << 16 | pixel << 8 | 0xff, false);
-                }
-            } else if (channels === 2) {
-                for (let i = 0; i < width * height * 2; i += 2) {
+        if (channels === 1) {
+            for (let i = 0; i < width * height; i++) {
+                const pixel = pixels[i];
+                view.setUint32(i * 4, pixel << 24 | pixel << 16 | pixel << 8 | 0xff, false);
+            }
+        } else if (channels === 2) {
+            for (let i = 0; i < width * height * 2; i += 2) {
                     const pixel = pixels[i];
                     view.setUint32(i * 2, pixel << 24 | pixel << 16 | pixel << 8 | pixels[i + 1], false);
                 }
