0f8b0a9545752b5b35de3bc2c9991d0eb5eb1936
Liamolucko <liampm32@gmail.com>
2021-01-12 19:27:35 +1100

    Update example not to use worker

--- example/runner.js
+++
@@ -1,4 +0,0 @@
-// we need to spawn a worker thread to compile large WASM (we don't want to be blocking the main thread)
-const worker = new Worker('worker.js');
-// worker threads dont have access to the document, so we just receive the blob URI and use it as src
-worker.onmessage = msg => document.querySelector('#image').src = msg.data;
