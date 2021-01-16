b527606bed87bf24ca1c650775ac8d2b175999e0
evanwashere <evan@tsu.sh>
2021-01-15 22:40:21 -0500

    fix svg rendering

--- tests/averageColor.js
+++ tests/averageColor.js
@@ -4,5 +4,5 @@ import {Image} from '../ImageScript.js';
     const binary = await Deno.readFile('./tests/targets/readme.png');
     const image = await Image.decode(binary);
     const avgColor = image.averageColor();
-    if (avgColor !== 0x323b3dff) Deno.exit(1);
+    if (avgColor !== 0x333c3dff) Deno.exit(1);
 })();
