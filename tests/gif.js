7480d6254c2a128d39e854cb3b933f832c557b11
Mathis Mensing <matmen@dreadful.tech>
2021-01-23 19:26:02 +0100

    GIF decoding
    (cherry picked from commit db1a0b79aaba38d492134f18e7724c9d943b1c05)

--- tests/gif.js
+++ tests/gif.js
@@ -1,5 +1,10 @@
 import {Frame, GIF} from '../ImageScript.js';
-import {equals} from "https://deno.land/std@0.80.0/bytes/mod.ts";
+import {equals} from 'https://deno.land/std@0.80.0/bytes/mod.ts';
+
+const panic = message => {
+    console.error(message);
+    process.exit(1);
+};
 
 (async () => {
     const frames = [];
@@ -11,7 +16,26 @@ import {equals} from "https://deno.land/std@0.80.0/bytes/mod.ts";
 
     const gif = new GIF(frames);
 
-    const encoded = await gif.encode();
-    const desired = await Deno.readFile('./tests/targets/gif.gif');
-    Deno.exit(equals(desired, encoded) ? 0 : 1);
+    {
+        const encoded = await gif.encode();
+        const desired = await Deno.readFile('./tests/targets/gif.gif');
+        if (!equals(desired, encoded))
+            panic('encoding failed');
+    }
+
+    {
+        const binary = await Deno.readFile('./tests/targets/gif.gif');
+        const decoded = await GIF.decode(binary);
+        if (decoded.width !== gif.width)
+            panic('decode: incorrect width');
+        if (decoded.height !== gif.height)
+            panic('decode: incorrect width');
+        if (decoded.length !== gif.length)
+            panic('decode: incorrect frame count');
+
+        for (let i = 0; i < gif.length; i++) {
+            if (gif[i].bitmap.length !== decoded[i].bitmap.length)
+                panic('decode: incorrect frame bitmap length');
+        }
+    }
 })();
