1a69f7bb3cf18993b671b995cff13e0f7f949948
evanwashere <evan@tsu.sh>
2021-03-18 11:13:57 -0400

    gif frame offset support
    (cherry picked from commit 3588e8f478c91e8f345cf7a2f54d7d43ce9decca)

--- ImageScript.js
+++ ImageScript.js
@@ -1348,7 +1348,7 @@ class GIF extends Array {
 
         for (const frame of this) {
             if (!(frame instanceof Frame)) throw new Error('GIF contains invalid frames');
-            encoder.add(~~(frame.duration / 10), frame.width, frame.height, frame.bitmap, quality);
+            encoder.add(~~(frame.duration / 10), 0, 0, frame.width, frame.height, frame.bitmap, quality);
         }
 
         return encoder.u8();
