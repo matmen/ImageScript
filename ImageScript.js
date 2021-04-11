59152fc4387dbf7bc39242addadf2344d99b33d1
evanwashere <evan@tsu.sh>
2021-04-11 12:48:49 -0400

    gif or jif
    (cherry picked from commit 46b181fc550477d6943624244468e98d04fb7e2c)

--- ImageScript.js
+++ ImageScript.js
@@ -1375,7 +1375,7 @@ export class GIF extends Array {
 
         for (const frame of this) {
             if (!(frame instanceof Frame)) throw new Error('GIF contains invalid frames');
-            encoder.add(frame.xOffset, frame.yOffset, ~~(frame.duration / 10), frame.width, frame.height, frame.bitmap, quality);
+            encoder.add(frame.xOffset, frame.yOffset, ~~(frame.duration / 10), frame.width, frame.height, frame.bitmap, 0, quality);
         }
 
         return encoder.u8();
