bdd37042709e54bc4443bb7bb97cb069ff3d83cf
Mathis Mensing <matmen@dreadful.tech>
2021-04-17 16:47:56 +0200

    gif/frame resize helper

--- ImageScript.js
+++ ImageScript.js
@@ -1378,6 +1378,18 @@ class Frame extends Image {
 
         return frame;
     }
+
+    resize(width, height, mode = Image.RESIZE_NEAREST_NEIGHBOR) {
+        const originalWidth = this.width;
+        const originalHeight = this.height;
+
+        const result = super.resize(width, height, mode);
+
+        this.xOffset *= result.width / originalWidth;
+        this.yOffset *= result.height / originalHeight;
+
+        return result;
+    }
 }
 
 /**
@@ -1394,9 +1406,6 @@ class GIF extends Array {
     constructor(frames, loopCount = -1) {
         super(...frames);
 
-        this.width = Math.max(...frames.map(frame => frame.width));
-        this.height = Math.max(...frames.map(frame => frame.height));
-
         for (const frame of this)
             if (!(frame instanceof Frame))
                 throw new TypeError(`Frame ${this.indexOf(frame)} is not an instance of Frame`);
@@ -1405,6 +1414,28 @@ class GIF extends Array {
             throw new RangeError('Invalid loop count');
 
         this.loopCount = loopCount;
+    }
+
+    get width() {
+        let max = 0;
+        for (const frame of this) {
+            let width = frame.width + frame.xOffset;
+            if (max < width)
+                max = width;
+        }
+
+        return max;
+    }
+
+    get height() {
+        let max = 0;
+        for (const frame of this) {
+            let height = frame.height + frame.yOffset;
+            if (max < height)
+                max = height;
+        }
+
+        return max;
     }
 
     toString() {
@@ -1481,6 +1512,11 @@ class GIF extends Array {
         } else throw new Error('Unsupported image type');
 
         return image;
+    }
+
+    resize(width, height, mode = Image.RESIZE_NEAREST_NEIGHBOR) {
+        for (const frame of this)
+            frame.resize(width, height, mode);
     }
 }
 
