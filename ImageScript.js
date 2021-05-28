045c035b3ae9f91d9f291f5f0f32e9ac73f4d914
Hideo Hattori <hhatto.jp@gmail.com>
2021-05-28 10:07:36 +0900

    fix: TextLayout.wrapHardBreaks's argument type on docs

--- ImageScript.js
+++ ImageScript.js
@@ -1665,7 +1665,7 @@ class TextLayout {
      * @param {string} [options.wrapStyle='word'] The texts wrap style when reaching the max width (word, char)
      * @param {string} [options.verticalAlign='left'] The vertical align mode (left, center, right)
      * @param {string} [options.horizontalAlign='top'] The horizontal align mode (top, middle, bottom)
-     * @param {string} [options.wrapHardBreaks=true] Whether to force wrap at new line characters
+     * @param {boolean} [options.wrapHardBreaks=true] Whether to force wrap at new line characters
      */
     constructor(options) {
         const {maxWidth, maxHeight, wrapStyle, verticalAlign, horizontalAlign, wrapHardBreaks} = options || {};
